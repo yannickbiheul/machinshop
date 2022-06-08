111fbb1b382d4e10d85d4793d9a0f08dd71c84c6
Yannick Biheul <yannick.biheul2022@campus-eni.fr>
2022-06-08 09:10:26 +0200

    balek

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(): Response
+    {
+        $title = "Machin Shop";
+        return $this->render('home/index.html.twig', [
+            'title' => $title,
+        ]);
+    }
+}
